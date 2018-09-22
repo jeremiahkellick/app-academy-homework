dd8f30d270dbe0ac3e76f99b39107c56dd45c434
Jeremiah Kellick <jeremiah@jeremiahkellick.com>
2018-09-22 16:50:55 -0700

    Finish Intro to Canvas

---
+++ W6D1/canvas_practice/practice.js
@@ -0,0 +1,21 @@
+document.addEventListener("DOMContentLoaded", function(){
+  const canvas = document.getElementById('mycanvas');
+  canvas.height = 500;
+  canvas.width = 500;
+  const ctx = canvas.getContext('2d');
+  ctx.fillStyle = 'red';
+  ctx.fillRect(0, 0, 500, 500);
+  ctx.beginPath();
+  ctx.arc(250, 250, 100, 0, 360);
+  ctx.strokeStyle = 'black';
+  ctx.lineWidth = 4;
+  ctx.stroke();
+  ctx.fillStyle = 'PapayaWhip';
+  ctx.fill();
+  ctx.beginPath();
+  ctx.moveTo(75, 50);
+  ctx.lineTo(200, 150);
+  ctx.lineTo(200, 25);
+  ctx.fillStyle = 'white';
+  ctx.fill();
+});
