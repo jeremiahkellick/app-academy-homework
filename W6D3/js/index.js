61e1f8259f22b3035376d0b2898d407d08e4a78d
Jeremiah Kellick <jeremiah@jeremiahkellick.com>
2018-09-25 20:45:10 -0700

    Finish AJAX Request

---
+++ W6D3/js/index.js
@@ -0,0 +1,13 @@
+console.log("Hello from the JavaScript console!");
+
+// Your AJAX request here
+$.ajax({
+  method: 'GET',
+  url: 'http://api.openweathermap.org/data/2.5/weather?q=new%20york,US&appid=bcb83c4b54aee8418983c2aff3073b3b',
+  success: function(response) {
+    console.log(response);
+  }
+});
+
+// Add another console log here, outside your AJAX request
+console.log('last in file');
